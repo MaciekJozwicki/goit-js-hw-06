8b556d329b8ad61646e7b087c81e7ebf14df6fc1
Maciek <118013298+MaciekJozwicki@users.noreply.github.com>
2023-03-23 14:56:37 +0100

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const listEl = document.getElementById("categories");
+
+// Zadanie 1 - 2 sposoby:
+const categoriesQuantity = listEl.children.length;
+// console.log(categoriesQuantity);
+const listItem = document.querySelectorAll(".item");
+const categoriesQuantity2 = listItem.length;
+console.log(
+    `Number of categories: ${categoriesQuantity2}`)
+
+listItem.forEach((element) => {
+    const itemTitle = element.querySelector("h2");
+    const itemQuantity = element.querySelectorAll("li")
+    console.log(`
+Category: ${itemTitle.textContent}
+Elements: ${itemQuantity.length}`);
+
+})
+
